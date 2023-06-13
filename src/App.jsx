08abb2135bb5cd987f454fce0c191591186bdd47
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-13 15:50:13 +0530

    creating a empty and basic folder structure for the whole appliaction

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { useState, useEffect } from 'react';
-import { fetchDataFromApi } from "./utils/api"; // Update the import statement
+import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
+
+import Header from "./components/header/Header";
+import Footer from "./components/header/Header";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/pageNotFound";
+
 
 function App() {
+  const dispatch = useDispatch();
+
+  const {url} = useSelector((state) => state.home);
+
   useEffect(() => {
     apiTesting();
   }, []);
@@ -9,16 +24,14 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi('/movie/popular')
       .then((res) => {
-        console.log(res);
+        dispatch(getApiConfiguration(res));
       })
-      .catch((error) => {
-        console.log(error);
-      });
   };
 
   return (
     <div>
       App
+      {url?.total_pages}
     </div>
   );
 }
