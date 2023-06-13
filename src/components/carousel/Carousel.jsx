acdf9d9425d5689fef4bed01dde55bb19a5b4957
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-13 17:24:52 +0530

    not loading page error fixed and created an updated code for genres and rating

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -9,9 +9,9 @@ import dayjs from "dayjs";
 
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 import Img from "../lazyLoadImage/Img";
-// import PosterFallback from "../../assets/no-poster.png";
-// import CircleRating from "../circleRating/CircleRating";
-// import Genres from "../genres/Genres";
+import PosterFallback from "../../assets/no-poster.png";
+import CircleRating from "../circleRating/CircleRating";
+import Genres from "../genres/Genres";
 
 import "./style.scss";
 
