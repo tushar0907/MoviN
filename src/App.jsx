c79aae5253a4cc2eb7492c932d27e35938649d04
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-13 16:49:29 +0530

    updated the herobanner file with styling and some minor supporting folder created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from 'react-redux';
 import { getApiConfiguration } from './store/homeSlice';
@@ -16,23 +17,38 @@ function App() {
   const dispatch = useDispatch();
 
   const {url} = useSelector((state) => state.home);
+  console.log(url);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
-      .then((res) => {
-        dispatch(getApiConfiguration(res));
-      })
-  };
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+        console.log(res);
+
+        const url = {
+            backdrop: res.images.secure_base_url + "original",
+            poster: res.images.secure_base_url + "original",
+            profile: res.images.secure_base_url + "original",
+        };
+
+        dispatch(getApiConfiguration(url));
+    });
+};
 
   return (
-    <div>
-      App
-      {url?.total_pages}
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/:mediaType/:id" element={<Details />} />
+      <Route path="/search/:query" element={<SearchResult />} />
+      <Route path="/explore/:mediaType" element={<Explore />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+    <Footer />
+    </BrowserRouter>
   );
 }
 
