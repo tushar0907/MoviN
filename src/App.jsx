9659da1d7070058cc77535c2eb8b4a666b6e011a
Tushar Kumar <tusharkumar0807@gmail.com>
2023-06-13 16:59:56 +0530

    created a header and footer functionality and updated the existing code

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getApiConfiguration } from './store/homeSlice';
 
 import Header from "./components/header/Header";
-import Footer from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
